55758090ea93730da6150d15a0beb2d5a0637c87
JacksonQWERTYFrontier <2025remy.fritz@fcboe.org>
2025-02-04 13:42:33 -0500

    ball.java

---
+++ Animation basics/ball.java
@@ -0,0 +1,9 @@
+class Ball {
+
+
+float x,y, radius, vx, vy, ax, ay;
+int fill_color;
+
+public Ball(flat, x , float y, float radius, float xv, float xy, float vy, float ax, float ay, int fill_color) {
+  this.x 
+}
